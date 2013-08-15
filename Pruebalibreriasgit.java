ff565264263acc81adf53bd3c2f0e3434c50d5f4
Arkadoel <fer.d.minguela@gmail.com>
2013-08-15 16:09:54 +0200

    lol

---
+++ Pruebalibreriasgit.java
@@ -0,0 +1,20 @@
+package pruebalibreriasgit;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+
+
+/**
+ *
+ * @author developer
+ */
+public class Pruebalibreriasgit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Repository repo = new Repository()
+        Git a = new Git(null);
+    }
+}
